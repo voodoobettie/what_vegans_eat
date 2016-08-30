588bf318a028d38b33393b83d38d9d285d389f3d
voodoobettie <ingrid@frankenmedia.ca>
2016-08-30 15:33:54 -0700

    initial commit

---
+++ outline.js
@@ -0,0 +1,165 @@
+<div>
+<h1>A reset I should’ve done a long time ago</h1>
+
+
+<blockquote>"Don't take it for granted because health is a thing you have to actively try and grasp at every day. Go beyond it, unlock the potential."
+</blockquote>
+
+<h2>Aims:</h2>
+<ul>
+<li>Lose excess body fat</li>
+<li>Gain strength, flexibility, agility and overall fitness</li>
+</ul>
+
+</div>
+
+
+recipes = new collection(){}; // or whatever
+
+// healthy breakfasts
+var breakfasts = [smoothie, "bircher museli",  ];
+
+var group_brekky = [];
+
+var weHave = [
+	// lentils.(boolean: true);
+	
+
+
+
+	// pantry
+	"vega protein powder - vanilla",
+	"vega protein powder- chocolate",
+	lentils,
+	"baking powder",
+	"baking soda",
+	"brown flour",
+	"white flour",
+	"vegetable stock",
+	"pumpkin seeds",
+
+	// freezer
+	blueberries,
+	blackberries,
+	tofu,
+	"hemp seeds",
+	"flax seeds",
+
+
+	// fridge
+
+	// produce
+
+}
+
+new recipe.prototype.method_name = function(name) {
+	// creating a prototype to build recipes
+	recipe.name = this.name;
+	//recipe.ingredients = [];
+	//recipe.preptime = 
+};
+
+var smoothie = new recipe {
+	// selection from berries based on availability (avail)
+	ingredients: //[function() {[ berries ]}]
+	// if (ingredients[i].avail == true;){
+		// push banana to meal plan
+		// greens
+		// almond milk
+
+	}
+
+}
+
+
+// communal breakfasts (Sat / Sun)
+
+
+// lunches
+// 	big salad
+	// broccoli
+	// pumpkin seeds
+
+
+
+// soup du jour
+	new recipe = {
+		name = "lentil soup"
+		ingredients = [lentils, "vegetable stock", 
+		};
+	}
+
+
+	"vegetable soup",
+	"carrot soup"
+
+// dinner
+	"stir fry"
+
+
+
+// treats
+	mocha cookies
+	crumble
+
+
+
+var days = [Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday];
+
+
+if (ingredients.avail == true) {
+	for (var i; i = 0; i++){
+	// var choice = function() {((Math.rand(1)*Math.floor); 
+	// however this goes
+	this.push(days[i]];
+};
+
+new Recipe(matbucha){
+    ingredients = [//peppers: "2 bell peppers",
+    "1 med jalapeño",
+    "10 lg (roma) tomatoes",
+    "1 clove garlic",
+    "1 1/2 tbsp agave syrup",
+    "1 tsp chili pepper flakes",
+    "1/2 tsp salt",
+    "1/2 cup olive oil",
+    "1 tbsp paprika"],
+    method = "
+
+	Roast the bell peppers. Seed peppers, peel off charred skin and chop.\n 
+
+	In saucepan, combine diced tomatoes, roasted bell peppers, minced jalapeño, 
+	garlic, sugar, chili pepper flakes, and salt. Bring to a simmer over medium 
+	high heat.\n
+
+	Reduce heat to medium low and simmer, stirring often. Cook for about 45 min.\n
+
+	In a small bowl, whisk together olive oil and paprika. Add the oil mixture 
+	into the Matbucha pan and stir. Cook for about 30 minutes more, stirring 
+	every 5 minutes.\n
+
+	Best served at room temperature or chilled with lots of bread for sopping."
+	
+};
+
+var hummus = new Recipe(){
+	ingredients = [
+"2 cups cooked chickpeas",
+"(1/4) cup lemon juice",
+"(1/4 cup orange juice",
+"¼ cup Tahini",
+"1 or 2 cloves garlic",
+"¼ cup olive oil",
+"1 tsp salt"
+],
+	method = "In food processor, add the juices, tahini, garlic and lemon. 
+	Blending well.<br/> Add in the chickpeas and salt, plus 1/4 cup olive oil 
+	to start, blending until desired consistency is reached. You may add more 
+	oil is necessary."
+};
+
+// shopping list
+
+var shopping_list = ["red peppers"]
+
+
